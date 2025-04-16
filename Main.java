eb51bb643292d39a57c9c7e08e506103798c9379
Valeria Melnyk <valeria1melnyk@gmail.com>
2025-04-16 11:00:29 +0300

    create and implementation some methods

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -28,7 +28,7 @@ public class Main {
     }
 
     public static Integer suma(List<Integer> numbers) {
-        return numbers.stream().reduce(0,(a,b)-> a+b);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static Integer product(List<Integer> numbers) {
@@ -40,6 +40,45 @@ public class Main {
                 .map(n->n*n)
                 .collect(Collectors.toList());
     }
+
+    public static List<String> sortWordsByLength(List<String> words) {
+        return words.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+
+    }
+
+    public static long countWordsInSentence(String sentence) {
+        return   Arrays.stream(sentence.split(" "))
+                .filter(word-> !word.isEmpty())
+                .count();
+    }
+
+    public static String findFirstNotEmptyWord(List<String> words) {
+        return words.stream().filter(word -> !word.isEmpty()).findFirst().get();
+    }
+
+    public static boolean allStartWithUpperCase(List<String> list) {
+        return list.stream()
+                .allMatch(s -> !s.isEmpty() && Character.isUpperCase(s.charAt(0)));
+    }
+
+    public static Optional<Integer> secondLargest(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst();
+    }
+
+    public static Optional<Integer> maxEvenNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .max(Integer::compareTo);
+    }
+
+
+
 
 
 
@@ -74,6 +113,33 @@ public class Main {
 
         int product = product(numbers1);
         System.out.println("Добуток чисел масиву: " +product);
+
+        List<Integer> squareNumbers = squareNumbers(numbers1);
+        System.out.println("Квадрати чисел масиву: " + squareNumbers);
+
+        List<String> sortWordsByLength = sortWordsByLength(words);
+        System.out.println("Сортування слів за довжиною: " + sortWordsByLength);
+
+        String sentense="Перевірка одного з методів";
+        long contWordsInSentense=countWordsInSentence(sentense);
+        System.out.println("Кількість слів "+contWordsInSentense);
+
+        List<String> lines= List.of("","1","");
+        String line1=findFirstNotEmptyWord(lines);
+        System.out.println("Знаходження першого не пустого рядка:"+ line1);
+
+        List<String> wordsWithDifferentCase=List.of("Pupupu","apple","Windows");
+        boolean wordsWithUpperCase=allStartWithUpperCase(wordsWithDifferentCase);
+        System.out.println("Всі слова з великої: " + wordsWithUpperCase);
+
+        List<Integer> numbers2= List.of(67,85,110,21,150,220);
+        Optional<Integer> secondLargest=secondLargest(numbers2);
+        System.out.println("Друге найбільше: " + secondLargest.get());
+
+        Optional<Integer> maxEven=maxEvenNumber(numbers2);
+        System.out.println("Максимальне парне: " + maxEven.get());
+
+
 
     }
 }
